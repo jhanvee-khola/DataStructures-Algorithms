0eb0da29427165877dbbdd5a0f07472f64780088
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2021-07-03 01:19:20 +0530

    Update isUnique.cpp

--- Strings/isUnique.cpp
+++ Strings/isUnique.cpp
@@ -1,3 +1,4 @@
+// Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? 
 bool isUnique(string s){
     for(int i=0;i<s.length();i++){
         if((s.find(s[i],i+1))!=string::npos){

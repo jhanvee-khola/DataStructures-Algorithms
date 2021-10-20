326a20c5b8f5b38b76781a11ce6754e44e7015c4
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2021-10-20 22:54:34 +0530

    Update StringTokenizer.cpp

--- Strings/StringTokenizer.cpp
+++ Strings/StringTokenizer.cpp
@@ -1,8 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void token(char* a, char delimiter){
-  string s;
+char* token(char* str, char delim){
+  static char* input=NULL;
   
+  if(str!=NULL){
+    input=str;
+  }
   
+  if(input==NULL){
+    return NULL;
+  }
+  char* output= new char[strlen(input)+1]
+  int i=0;
+  for(;input[i]!='\0';i++){
+    if(input[i]!=delim){
+      output[i]=input[i];
+    }
+    else{
+      output[i]='\0';
+      input=input+i+1;
+      return output;
+    }
+  }
+  output[i]='\0';
+  input=NULL:
+  return output;
+}
+
+int main(){
+  char s[];
+  cin.getline(s,100);
+  
+  char* ptr=token(s,' ');
+  cout<<ptr<<endl;
+  while(ptr!=NULL){
+    ptr=token(NULL,' ');
+    cout<<ptr<<endl;
+  }
+  return 0;
 }

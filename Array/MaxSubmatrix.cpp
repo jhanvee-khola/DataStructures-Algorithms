24466dab30db9d1421c8c9f4257f2c748ba806cc
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2021-10-22 02:40:07 +0530

    Update MaxSubmatrix.cpp
    O(m*n) time solution

--- Array/MaxSubmatrix.cpp
+++ Array/MaxSubmatrix.cpp
@@ -12,31 +12,25 @@ int main() {
 	        cin>>a[i][j];
 	    }
 	}
-	//Pre-processing to make a prefix sum matrix
+	//Pre-processing to make a suffix sum matrix since we want the bottom-right corner to be included
 	//column-wise
-	for(int i=0;i<n;i++){
-	    for(int j=0;j<m;j++){
-	        if(j!=0){
-	            a[i][j]+=a[i][j-1];
-	        }
-	        
+	for(int i=n-1;i>=0;i--){
+	    for(int j=m-2;j>=0;j--){
+	        a[i][j]+=a[i][j+1];
 	    }
 	}
 	//row-wise
-	for(int j=0;j<m;j++){
-	    for(int i=0;i<n;i++){
-	        if(j!=0){
-	            a[i][j]+=a[i-1][j];
-	        }
-	        
+	for(int j=m-1;j>=0;j--){
+	    for(int i=n-2;i>=0;i--){
+	        a[i][j]+=a[i+1][j];
 	    }
 	}
-	
+	int maxSum=INT_MIN;
 	for(int i=n-1;i>=0;i--){
 	    for(int j=m-1;j>=0;j--){
-	        
+	        maxSum=max(a[i][j],maxSum);
 	    }
 	}
-	
+	cout<<maxSum<<endl;
 	return 0;
 }

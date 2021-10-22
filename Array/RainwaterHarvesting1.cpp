b352174b2f184653a8284a60660bc3d2aeb9e01b
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2021-10-23 00:57:53 +0530

    Update RainwaterHarvesting1.cpp

--- Array/RainwaterHarvesting1.cpp
+++ Array/RainwaterHarvesting1.cpp
@@ -1,24 +1,23 @@
-int maxWater(int arr[], int n){
+int maxWater(int arr[],int n){
 
 // To store the maximum water  
 // that can be stored 
-int res = 0; 
+int res=0; 
 
 // For every element of the array 
 for (int i=1;i<n-1;i++) { 
     // Find the maximum element on its left 
-    int left = arr[i]; 
+    int left=arr[i]; 
     for (int j=0;j<i;j++) 
-       left = max(left, arr[j]); 
+       left=max(left, arr[j]); 
 
     // Find the maximum element on its right    
-    int right = arr[i]; 
+    int right=arr[i]; 
     for (int j=i+1; j<n; j++) 
-       right = max(right, arr[j]);  
+       right=max(right,arr[j]);  
 
    // Update the maximum water     
-   res = res + (min(left, right) - arr[i]);    
+   res=res+(min(left,right)-arr[i]);    
 } 
-
 return res;  
 }

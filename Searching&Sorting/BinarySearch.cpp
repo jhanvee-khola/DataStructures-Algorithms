5418ab4ada9bf233e314e83c79431aac16e9dd89
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2021-11-12 11:21:07 +0530

    Create BinarySearch.cpp

---
+++ Searching&Sorting/BinarySearch.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namepsace std;
+
+int binarySearch(int a[],int s,int e,int k){
+  while(s<=e){
+    int mid=s+(e-s)/2;
+    if(a[mid]==k){
+      return mid;
+    }
+    else if(a[mid]<k){
+      return binarySearch(a,mid+1,e,k);
+    }
+    else{
+      return binarySearch(a,s,mid-1,k);
+    }
+  }
+  return -1;
+}
+
+int main(){
+  int n;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+  }
+  int k;
+  cin>>k;
+  
+  return 0;
+}

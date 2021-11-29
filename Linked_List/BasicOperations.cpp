b138b56d8fa3546d824363d6eaa37647b8b15a5c
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2021-11-30 02:28:53 +0530

    Create BasicOperations.cpp

---
+++ Linked_List/BasicOperations.cpp
@@ -0,0 +1,31 @@
+#inculde<iostream>
+using namespace std;
+
+//Creating
+class node{
+  public:
+    int data;
+    node* next;
+  
+    node(int d){
+      data=d;
+      next=NULL:
+    }
+};
+
+//Insertion
+void insertAtHead(node*&head, int d){
+  
+}
+void insertAtTail(node*&head, int d){
+  
+}
+void insertInMiddle(node*&head, int d){
+
+}
+
+//Printing
+void print(node*head){
+  
+}
+//Deletion

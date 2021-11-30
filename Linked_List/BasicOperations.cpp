e12cb10f4bf3bbfe0df105cc2fe7324d411298a4
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2021-11-30 22:49:24 +0530

    Update BasicOperations.cpp

--- Linked_List/BasicOperations.cpp
+++ Linked_List/BasicOperations.cpp
@@ -13,19 +13,73 @@ class node{
     }
 };
 
+//Length
+int length(node*head){
+  int l=0;
+  while(head!=NULL){
+    head=head->next;
+    l++;
+  }
+  return l;
+}
+
 //Insertion
 void insertAtHead(node*&head, int d){
-  
+  if(head==NULL){
+    head=new node(d);
+    return;
+  }
+  node* n= new node(d);
+  n->next=head;
+  head=n;
 }
 void insertAtTail(node*&head, int d){
-  
+  if(head==NULL){
+    head=new node(d);
+    return;
+  }
+  node*tail=head;
+  while(tail->next!=NULL){
+    tail=tail->next;
+  }
+  head->next=new node(d);
 }
-void insertInMiddle(node*&head, int d){
-
+void insertInMiddle(node*&head,int d,int p){
+  if(head==NULL || p==0){
+    insertAtHead(head,d);
+  }
+  else if(p>length(head)){
+    insertAtTail(head,d);
+  }
+  else{
+    int jump=1;
+    node* n= new node(d);
+    node* temp=head;
+    while(jump<p){
+      temp=temp->next;
+      jump++;
+    }
+    n->next=temp->next;
+    temp->next=n;
+  }
 }
 
 //Printing
 void print(node*head){
+  while(head!=NULL){
+    cout<<head->data<<" ";
+    head=head->next;
+  }
+  cout<<endl;
+}
+
+//Deletion
+void deleteHead(){
   
 }
-//Deletion
+void deleteTail(){
+
+}
+void deleteMiddle(){
+
+}

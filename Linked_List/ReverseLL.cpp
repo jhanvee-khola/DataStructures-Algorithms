e9d490a1adf1080e636effaf3aaae292f4e7b117
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2021-12-13 14:31:14 +0530

    Update ReverseLL.cpp

--- Linked_List/ReverseLL.cpp
+++ Linked_List/ReverseLL.cpp
@@ -1,3 +1,18 @@
+#inculde<iostream>
+using namespace std;
+
+//Creating
+class node{
+  public:
+    int data;
+    node* next;
+  
+    node(int d){
+      data=d;
+      next=NULL:
+    }
+};
+
 //Iterative Approach
 void reverse(node*&head){
   node* curr=head,prev=NULL,temp;;

fcf5a9157f3718c848c934da4d59c04f550d2498
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-02-27 21:30:28 +0530

    Create Expedition.cpp

---
+++ Greedy/Expedition.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+bool compare(pair<int,int> p,pair<int,int> q){
+  return 
+}
+
+int expedition(int l,int n,int p,vector<pair<int,int> f){
+  priority_queue<int> pq;
+  sort(f.begin(),f.end(),compare);
+  
+  
+}

695c3fddacb8ec09f60577ac85257297b02994ed
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-03-07 16:36:55 +0530

    Update 0-1Knapsack.cpp

--- DynamicProgramming/0-1Knapsack.cpp
+++ DynamicProgramming/0-1Knapsack.cpp
@@ -38,11 +38,11 @@ int knapsack2(int w[],int v[],int n,int c){
       if(i==0 || j==0){
         dp[i][j]=0;
       }
-      else if(w[i-1]>c){
+      else if(w[i-1]>j){
         dp[i][j]=dp[i-1][j];
       }
       else{
-        dp[i][j]=max(val[i-1]+dp[i-1][c-w[i-1]],dp[i-1][j]);
+        dp[i][j]=max(val[i-1]+dp[i-1][j-w[i-1]],dp[i-1][j]);
       }
     }
   }

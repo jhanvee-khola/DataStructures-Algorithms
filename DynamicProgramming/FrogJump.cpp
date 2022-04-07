c259892a27c0d375a6e3b17355bebff5a82c37df
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-04-07 10:57:41 +0530

    Update FrogJump.cpp

--- DynamicProgramming/FrogJump.cpp
+++ DynamicProgramming/FrogJump.cpp
@@ -1,3 +1,4 @@
+//Recursive Approach
 int frogJump(int a[],int n){
   if(n==0){
     return n;
@@ -10,6 +11,7 @@ int frogJump(int a[],int n){
   return min(l,r);
 }
 
+//top-down dp
 int frogJump(int a[],int n,int dp[]){
   if(n==0){
     return n;
@@ -25,6 +27,7 @@ int frogJump(int a[],int n,int dp[]){
   return dp[n]=min(l,r);
 }
 
+//bottom-up dp
 int frogJump(int a[],int n){
   int dp[n];
   dp[0]=0;
@@ -37,6 +40,7 @@ int frogJump(int a[],int n){
   return dp[n-1]
 }
 
+//space-optimised dp
 int frogJump(int a[],int n){
   int a=0,b=abs(a[1]-a[0]);
   for(int i=2;i<n;i++){
@@ -47,4 +51,35 @@ int frogJump(int a[],int n){
     b=dp[i];
   }
   return dp[n-1]
+}
+
+//follow-up question: If frog can take 1,2,....k steps at a time, find the solution
+//recursive solution
+int frogJump(int a[],int n,int k){
+  if(n==0){
+    return 0;
+  }
+  int ans=INT_MAX;
+  for(int i=1;i<=k;i++){
+    int e=frogJump(a,n-j)+abs(a[n]-a[n-j]);
+    ans=min(ans,e);
+  }
+  return ans;
+}
+
+//bottom-up dp;
+int frogJump(int a[],int n,int k){
+  int dp[n];
+  dp[0]=0;
+  for(i=1;i<n;i++){
+    int minSteps=INT_MAX;
+    for(int j=1;j<=k;j++){
+      if(i-j>=0){
+        int e=dp[i-j]+abs(a[i]-a[i-j]);
+        minSteps=min(minSteps,e);
+      }
+    }
+    dp[i]=minSteps;
+  }
+  return dp[n-1];
 }

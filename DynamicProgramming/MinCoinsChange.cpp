4f6c91603e2c537c8479a3dfdc7d7e7616bd6d78
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-04-14 22:06:44 +0530

    Update MinCoinsChange.cpp

--- DynamicProgramming/MinCoinsChange.cpp
+++ DynamicProgramming/MinCoinsChange.cpp
@@ -1,42 +1,70 @@
-//top-down dp - O(n*t) time
-int mcc(int n,int coins[],int t,int dp[]){
-  if(n==0){
-    return 0;
-  }
-  if(dp[n]!=0){
-    return 0;
-  }
-  int ans=INT_MAX;
-  for(int i=0;i<t;i++){
-    if(n-coins[i]>=0){
-      int d=mcc(n-coins[i],coins,t,dp);
-      ans=min(ans,d)+1;
+//Given an array of available denomination of coins, find the minimum no. of coins required to make the given sum of money
+
+//recursive approach
+int mcc(int n,int a[],int k){
+  if(n==1){
+    if(k%a[0]==0){
+      return k/a[0];
     }
-    else{
-      break;
-    }
+    return 1e9;
   }
-  dp[n]=ans;
-  return dp[n];
+  int nt=mcc(n-1,a,k);
+  int t=INT_MAX;
+  if(a[n-1]<=k){
+    t=1+mcc(n,a,k-a[n-1]);
+  }
+  return min(t,nt);
 }
 
-//bottom-up dp - O(n*t) time
-int mcc1(int n,int coins[],int t){
-  if(n==0){
-    return 0;
-  }
-  int dp[1000];//assuming n<1000
-  for(int i=1;i<n;i++){
-    dp[i]=INT_MAX;
-    for(int j=0;j<t;j++){
-      if(n-coins[j]>=0){
-        int d=dp[n-coins[j]];
-        dp[i]=min(d,dp[i])+1;
-      }
-      else{
-        break;
-      }
+//top-dpwn dp
+int mcc(vector<int>& a,int n,int k,vector<vector<int>> dp){
+    if(n==1){
+        if(k%a[0]==0){
+            return k/a[0];
+        }
+        return 1e9;
     }
-  }
-  return dp[n];
+    if(dp[n][k]!=-1){
+        return dp[n][k];
+    }
+    int nt=mcc(a,n-1,k,dp);
+    int t=INT_MAX;
+    if(a[n-1]<=k){
+        t=1+mcc(a,n,k-a[n-1],dp);
+    }
+    return dp[n][k]=min(t,nt);
+}
+
+//bottom-up dp
+int mcc(vector<int>& a, int k) {
+    int n=a.size();
+    int dp[n+1][k+1];
+    for(int i=0;i<=n;i++){
+        dp[i][0]=0;
+    }
+    for(int i=1;i<=k;i++){
+        if(i%a[0]==0){
+            dp[1][i]=i/a[0];
+        }
+        else{
+            dp[1][i]=1e9;
+        }
+    }
+    for(int i=1;i<=k;i++){
+        dp[0][i]=-1;
+    }
+    for(int i=2;i<=n;i++){
+        for(int j=1;j<=k;j++){
+            int nt=dp[i-1][j];
+            int t=INT_MAX;
+            if(a[i-1]<=j){
+                t=1+dp[i][j-a[i-1]];
+            }
+            dp[i][j]=min(t,nt);
+        }
+    }
+    if(dp[n][k]>=1e9){
+        dp[n][k]=-1;
+    }
+    return dp[n][k];
 }

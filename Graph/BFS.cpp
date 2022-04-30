50eca35fe31f6506c40b100f6669c932b0313dcb
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-05-01 02:18:29 +0530

    Create BFS.cpp

---
+++ Graph/BFS.cpp
@@ -0,0 +1,24 @@
+vector<int> bfsGraph(int n,vector<int> adj[]){
+  vector<int> bfs;
+  vcetor<int> visit(n,0);
+  
+  for(int i=1;i<=n;i++){
+    if(!visit[i]){
+      queue<int> q;
+      q.push(i);
+      visit[i]=1;
+      while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        bfs.push_back(node);
+        for(auto it: adj[node]){
+          if(!visit[it]){
+            q.push(it);
+            visit[it]=1;
+          }
+        }
+      }
+    }
+  }
+  return bfs;
+}

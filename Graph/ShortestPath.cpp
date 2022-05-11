6d62528d48c11fd867b1f0f10351c476e6db52f5
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-05-11 12:30:42 +0530

    Update ShortestPath.cpp

--- Graph/ShortestPath.cpp
+++ Graph/ShortestPath.cpp
@@ -18,4 +18,31 @@ vector<int> shortestPath(vector<pair<int,int>> adj[],int n,int s){
     }
   }
   return d;
-}
+}//Dijkstra's Algorithm doesn't work in case of negative edges
+
+//Bellman-Ford Algorithm
+void shortestPath(vector<vector<int>> edg,int n,int s){
+  vector<int> d(n,INT_MAX);
+  d[s]=0;
+  //relaxing the edges n-1 times
+  for(int i=1;i<=n-1;i++){
+    for(auto it: edg){
+      if(d[it[0]]+it[2]<d[it[1]]){
+        d[it[1]]=d[it[0]]+it[2];
+      }
+    }
+  }
+  bool f=false;
+  for(auto it: edg){
+    if(d[it[0]]+it[2]<d[it[1]]){
+      cout<<"Negative Cycle";
+      f=true;
+      break;
+    }
+  }
+  if(!f){
+    for(int i=0;i<n;i++){
+      cout<<i<<" "<<d[i]<<endl;
+    }
+  }
+}//Bellman-Ford Algorithm doesn't work in case of presence of negative cycle

8487ed12bb789fe254774eef7bf5d296e38fcf86
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-07-04 01:06:29 +0530

    Update WorthyKingII.cpp

--- Greedy/WorthyKingII.cpp
+++ Greedy/WorthyKingII.cpp
@@ -1,3 +1,5 @@
+
+//-----------------------------------------------------------
 #define ll long long int
 
 ll worthy(int n,vector<ll> nTroops){
@@ -26,3 +28,23 @@ ll worthy(int n,vector<ll> nTroops){
   }
   return total;
 }
+
+//---------------------------------------------------------------------------
+#define ll long long int
+
+ll worthy(int n,vector<ll> nTroops){
+  sort(nTroops.begin(),nTroops.end());
+  ll total=nTroops[0];
+  ll x=2;
+  for(int i=1;i<n;i++){
+    total+=nTroops[i]/x;
+    if(nTroops[i]%x){
+      total++;
+    }
+    x++;
+  }
+  return total;
+}
+
+//-----------------------------------------------------------------------------
+

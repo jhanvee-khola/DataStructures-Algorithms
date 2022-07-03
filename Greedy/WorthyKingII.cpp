84d8f857f695033d62701130e5cd836bef9ae8b0
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-07-03 22:29:49 +0530

    Update WorthyKingII.cpp

--- Greedy/WorthyKingII.cpp
+++ Greedy/WorthyKingII.cpp
@@ -2,14 +2,27 @@
 
 ll worthy(int n,vector<ll> nTroops){
   sort(nTroops.begin(),nTroops.end());
-  ll total=nTroops[0];
-  ll x=2;
-  for(int i=1;i<n;i++){
-    total+=nTroops[i]/x;
-    if(nTroops[i]%x){
-      total++;
+  vector<bool> attack(n,false);
+  int total=0;
+  for(int i=1;i<=n;i++){
+    bool b=false;
+    for(int j=0;j<n;j++){
+      if(!attack[j] && nTroops[j]%i==0){
+        total+=nTroops[j]/i;
+        b=true;
+        attack[j]=true;
+        break;
+      }
     }
-    x++;
+    if(!b){
+      for(int j=0;j<n;j++){
+        if(!attack[j]){
+          total+=nTroops[j]/i+1;
+          attack[j]=true;
+          break;
+        }
+      }
+    }
   }
   return total;
 }

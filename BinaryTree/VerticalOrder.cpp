ef42754754a60cfef4eccc11fea124edf7044d70
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-07-12 01:25:19 +0530

    Update VerticalOrder.cpp

--- BinaryTree/VerticalOrder.cpp
+++ BinaryTree/VerticalOrder.cpp
@@ -4,24 +4,23 @@ vector<vector<int>> verticalOrderTraversal(TreeNode *root){
     queue<pair<TreeNode*,int>> q;
     q.push({root,0});
     while(!q.empty()){
-        ans[]=;
         int s=q.size();
         for(int i=0;i<s;i++){
-            TreeeNode* node=q.front().first;
+            TreeNode* node=q.front().first;
             int hz=q.front().second;
             maxi=max(maxi,hz);
             mini=min(mini,hz);
             q.pop();
             if(mp.find(hz)==mp.end()){
                 vector<int> levelNodes;
-                mp.insert({hz,levelNodes})
+                mp.insert({hz,levelNodes});
             }
             mp[hz].push_back(node->val);
             if(node->left){
-                q.push(node->left,hz-1);
+                q.push({node->left,hz-1});
             }
             if(node->right){
-                q.push(node->right,hz+1);
+                q.push({node->right,hz+1});
             }
         }
     }

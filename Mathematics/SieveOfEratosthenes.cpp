e03c2cffe6a7c2c449f90fbd07e64c7ae21f0135
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2023-01-17 01:32:07 +0530

    Update SieveOfEratosthenes.cpp

--- Mathematics/SieveOfEratosthenes.cpp
+++ Mathematics/SieveOfEratosthenes.cpp
@@ -1,1 +1,20 @@
+//all primes upto n
 
+vector<int> sieveOfEratosthenes(int n){
+    vector<bool> sieve(n+1,true);
+    sieve[0]=sieve[1]=false;
+    for(int i=2;i<n;i++){
+        for(int j=2*i;j<=n;j+=i){
+            if(j%i==0){
+                sieve[j]=false;
+            }
+        }
+    }
+    vector<int> ans;
+    for(int i=2;i<=n;i++){
+        if(sieve[i]){
+            ans.push_back(i);
+        }
+    }
+    return ans;
+}
